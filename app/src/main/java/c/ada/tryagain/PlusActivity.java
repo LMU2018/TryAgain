a3ff94f52be487850639b2f9c55c045ad34b13f1
decand <deniejoey@gmail.com>
2019-01-16 10:18:24 +0700

    textview

--- app/src/main/java/c/ada/tryagain/PlusActivity.java
+++ app/src/main/java/c/ada/tryagain/PlusActivity.java
@@ -2,12 +2,16 @@ package c.ada.tryagain;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PlusActivity extends AppCompatActivity {
 
+    TextView a;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plus);
+
+        //test haha wkwk
     }
 }
