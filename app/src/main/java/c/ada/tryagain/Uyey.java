5db8f10e28010382471c23eb3c9695d05fd6d31a
LMU2018 <langgengmitrautama@gmail.com>
2019-01-18 10:41:57 +0700

    Uyey Activity new Branch

--- app/src/main/java/c/ada/tryagain/Uyey.java
+++ app/src/main/java/c/ada/tryagain/Uyey.java
@@ -9,5 +9,7 @@ public class Uyey extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uyey);
+        //vcs baru nih
+
     }
 }
